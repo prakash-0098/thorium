2107b65de8294baef54303c9ab7b3d5c28d64a61
Prakash Sah <prakashsah10091@gmail.com>
2022-02-21 16:34:24 +0530

    test

---
+++ src/routes/logger.js
@@ -0,0 +1,6 @@
+let url = "http://test.com"; 
+function log(message){
+    console.log(message); 
+}
+module.exports.endPoint = url; 
+module.exports.printMessage = log; 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const obj = require('./logger'); 
 
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
+    obj.printMessage('Prakash'); 
+    console.log(obj.endPoint); 
 });
 
 module.exports = router;
