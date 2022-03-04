2213c74cbbdcc0835e2ddaa4e660bd720200407e
Prakash Sah <prakashsah10091@gmail.com>
2022-03-04 15:58:28 +0530

    Task completed !

--- src/index.js
+++ src/index.js
@@ -4,18 +4,25 @@ const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
 
+const moment = require('moment'); 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://prakash:MD8GREj5x0NTptFX@cluster0.fg71d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use('/', route);
+app.use(function(request, response, next){
+    const logs = moment().format()+", "+request.socket.remoteAddress+", "+request.url; 
+    console.log(logs); 
+    next();
+}); 
 
+app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
