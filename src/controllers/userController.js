5dd36f25074e6e4da582446e45febeff448d5de5
Prakash Sah <prakashsah10091@gmail.com>
2022-03-10 21:42:56 +0530

    Task completed !

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,62 +2,92 @@ const userModel = require("../models/userModel");
 const jwt = require('jsonwebtoken'); 
 
 const createUser = async (request, response)=>{
-  const data = request.body; 
-  const dataRes = await userModel.create(data); 
-  response.send({
-    'msg': dataRes
-  }); 
+  try{
+    const data = request.body; 
+    const dataRes = await userModel.create(data); 
+    response.status(201).send({
+      'msg': dataRes
+    }); 
+  }catch(error){
+    response.status(500).send({
+      'Error :': error.message
+    }); 
+  }
 }
 
 const loginUser = async (request, response)=>{
-  const data = request.body; 
-  const dataRes = await userModel.findOne(data);
-  if(!dataRes){
-    return response.send({
-      'status': false,
-      'message': 'username or the password is not corerct'
+  try{
+    const data = request.body; 
+    const dataRes = await userModel.findOne(data);
+    if(!dataRes){
+      return response.status(401).send({
+        'status': false,
+        'message': 'username or the password is not corerct'
+      }); 
+    } 
+    const token = jwt.sign({
+      userId: dataRes._id.toString(),
+    }, '12345');
+    response.setHeader("x-auth-token", token); 
+    response.status(201).send({
+      'status': true,
+      'token': token
     }); 
-  } 
-  const token = jwt.sign({
-    userId: dataRes._id.toString(),
-  }, '12345');
-  response.setHeader("x-auth-token", token); 
-  response.send({
-    'status': true,
-    'token': token
-  });  
+  }catch(error){
+    response.status(500).send({
+      'Error :': error.message
+    });
+  }
 }
 
 const getUserData = async (request, response)=>{
-  const userId = request.params.userId; 
-  const dataRes = await userModel.findById(userId); 
-  response.send({
-    'status': true,
-    'msg': dataRes
-  }); 
+  try{
+    const userId = request.params.userId; 
+    const dataRes = await userModel.findById(userId); 
+    response.status(200).send({
+      'status': true,
+      'msg': dataRes
+    }); 
+  }catch(error){
+    response.status(500).send({
+      'Error :': error.message
+    });
+  }
 }
 
 const updatedUser = async (request, response)=>{
-  const userId = request.params.userId; 
-  const data = request.body; 
-  const dataRes = await userModel.findByIdAndUpdate(userId, data, { new: true}); 
-  response.send({
-    'status': true, 
-    'msg': dataRes
-  }); 
+  try{
+    const userId = request.params.userId; 
+    const data = request.body; 
+    const dataRes = await userModel.findByIdAndUpdate(userId, data, { new: true}); 
+    response.status(200).send({
+      'status': true, 
+      'msg': dataRes
+    }); 
+  }catch(error){
+    response.status(500).send({
+      'Error :': error.message
+    });
+  }
 }
 
 const userDelete = async (request, response)=>{
+  try{
     const userId = request.params.userId; 
     const dataRes = await userModel.findByIdAndUpdate(userId, {
       isDeleted: true
     }, {
       new: true
     }); 
-    response.send({
+    response.status(200).send({
       'status': true, 
       'msg': dataRes
     }); 
+  }catch(error){
+    response.status(500).send({
+      'Error :': error.message
+    });
+  }
 }
 
 module.exports = {
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,21 +4,21 @@ const auth = (request, response, next) => {
     const userId = request.params.userId;
     const token = request.headers['x-auth-token'];
     if (!token) {
-        response.send({
+        response.status(400).send({
             'status': false,
             'msg': 'token must be present'
         });
     }
     const decodedToken = jwt.verify(token, '12345');
     if (!decodedToken){
-        return response.send({
+        return response.status(400).send({
             status: false,
             msg: "token is invalid"
         });
     }
     else{
         if(decodedToken.userId != userId){
-            return response.send({
+            return response.status(403).send({
                 status: false,
                 msg: "User not authorized !"
             });
